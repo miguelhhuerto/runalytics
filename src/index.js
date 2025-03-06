2e38e24c2878a116fbc835a4f84b233411f5710d
miguelhhuerto <miguelhhuerto@gmail.com>
2025-03-06 15:50:18 +0800

    fix index.js again

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';  // ✅ Import the existing App component
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
@@ -11,9 +11,8 @@ Amplify.configure(awsconfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />  {}
+    <App />
   </React.StrictMode>
 );
-
 
 reportWebVitals();
